d2681cc291eb68c8bd11a7315bffbd92f09644c6
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2023-06-15 21:07:45 +0300

    feat: solution cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,28 @@
+import process from 'process';
+
 const parseArgs = () => {
     // Write your code here 
+
+    // console.log('arg', process.argv);
+
+    const args = process.argv.slice(2);
+
+    //console.log(args);
+
+    const arg1 = args.filter((_, i) => i %2 === 0);
+
+    const arg2 = args.filter((_, i) => i %2 !== 0);
+
+    //console.log(arg1.toString());
+    const result = [];
+    arg1.forEach((el, i)=> {
+            //console.log(`${el.slice(2)} is ${arg2[i]}`) 
+            result.push(` ${el.slice(2)} is ${arg2[i]}`)  
+   });
+
+   console.log(result.toString())
+
+
 };
 
 parseArgs();
